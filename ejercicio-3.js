17cd9a50da3da586c9f8d1d0ffb3c16d20050d6e
Sebastian Vega <sebaschanvega@gmail.com>
2023-04-22 15:40:43 -0500

    Tercer ejercicio

---
+++ ejercicio-3.js
@@ -0,0 +1,6 @@
+/*Escribe un programa que le pida al usuario ingresar un número, luego le pide un segundo número, e imprima en la consola la suma de los dos números que ingresó el usuario.
+*/
+
+let num1 = parseInt(prompt("Escribe un numero"))
+let num2 = parseInt(prompt("Escribe otro numero"))
+console.log(num1 + num2)
