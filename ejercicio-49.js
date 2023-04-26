8070cfba9c32ff175bf4f96da106ad063a993768
Sebastian Vega <sebaschanvega@gmail.com>
2023-04-26 17:55:21 -0500

    vacio

---
+++ ejercicio-49.js
@@ -0,0 +1,7 @@
+/*Escribe una función descifrar que reciba un string y un objeto. 
+Utiliza las propiedades del objeto para reemplazar los caracteres de la cadena y retorna el resultado:
+
+descifrar("h0l4", { 0: "o", 4: "a" }); // "hola"
+descifrar("pyrmizo", { y: "e", z: "s" }); // "permiso"
+descifrar("igual", { h: "n" }); // "igual" */
+
