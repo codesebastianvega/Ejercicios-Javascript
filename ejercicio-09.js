4832c85033eff812611d09e29ae8bb4984d67a44
Sebastian Vega <sebaschanvega@gmail.com>
2023-04-27 18:33:36 -0500

    Numero mayor-menor o igual a 10 (2)

--- ejercicio-09.js
+++ ejercicio-09.js
@@ -6,11 +6,11 @@ Si el número es mayor a 10 debe imprimir "El número es mayor que 10".
 
 Si el número es igual a 10 debe imprimir "El número es igual a 10".*/
 
-let num = parseInt(prompt("Escribe un numero "))
+let num = parseInt(prompt("Escribe un numero "));
 if (num < 10) {
-    console.log("El numero es menor a 10")
+  console.log("El numero es menor a 10");
 } else if (num > 10) {
-    console.log("El numero es mayor a 10")
+  console.log("El numero es mayor a 10");
 } else if (num === 10) {
-    console.log("El numero es igual a 10")
+  console.log("El numero es igual a 10");
 }
