1ccfb950c26345baf4dac02bc332984e7942acf0
Sebastian Vega <sebaschanvega@gmail.com>
2023-04-26 20:09:17 -0500

    Descifrado

--- ejercicio-49.js
+++ ejercicio-49.js
@@ -5,3 +5,25 @@ descifrar("h0l4", { 0: "o", 4: "a" }); // "hola"
 descifrar("pyrmizo", { y: "e", z: "s" }); // "permiso"
 descifrar("igual", { h: "n" }); // "igual" */
 
+
+// objeto con las letras a descifrar
+let frase = {
+    0: "o",
+    4: "a"
+}
+
+function descifrar (cadena, obj) {
+    let resultado = cadena;
+
+    for(const llave in obj) {
+         resultado = resultado.split(llave).join(obj[llave])
+    }
+
+    return resultado
+
+}
+
+console.log(descifrar("h0l4", frase))
+console.log(descifrar("pyrmizo", { y: "e", z: "s" }))
+console.log(descifrar("igual", { h: "n" }))
+
