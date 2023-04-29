47a94fdcc939691d6ddf0d9f748eab84a8bac33b
Sebastian Vega <sebaschanvega@gmail.com>
2023-04-28 19:56:54 -0500

    Piedra, Papel o Tijeras

---
+++ Platzi/Juego-de-Tetris.js
@@ -0,0 +1,26 @@
+//Juego piedra papel y tijera
+
+const opcionesCpu = ["tijeras", "papel", "piedra"];
+const cpu = opcionesCpu[Math.floor(Math.random() * opcionesCpu.length)];
+
+function game(jugador, cpu) {
+  if (jugador === cpu) {
+    resultado = "¡Es un empate!";
+  } else if (
+    (jugador === "piedra" && cpu === "tijeras") ||
+    (jugador === "papel" && cpu === "piedra") ||
+    (jugador === "tijeras" && cpu === "papel")
+  ) {
+    resultado = `¡Ganaste! ${jugador} vence a ${cpu}.`;
+  } else {
+    resultado = `¡Perdiste! ${cpu} vence a ${jugador}.`;
+  }
+
+  console.log(resultado);
+}
+
+// Escoge entre "piedra", "papel", "tijeras":
+let selec = "papel";
+
+// Llamar a la función con la selección del usuario y la selección aleatoria de la CPU
+game(selec, cpu);
