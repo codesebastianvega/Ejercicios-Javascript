753dc5475977105ebc086ee74f45ab033b770ce0
Sebastian Vega <sebaschanvega@gmail.com>
2023-05-02 18:07:05 -0500

    Año de nacimiento

--- Make It Real 1/ejercicio-04.js
+++ Make It Real 1/ejercicio-04.js
@@ -1,4 +1,4 @@
-/* Escribe un programa que le pida al usuario su año de nacimiento e imprima su edad actual en la consola con la frase "Tienes X años". Por ejemplo, asumiendo que el año actual es 2019 y el usuario ingresa 1999, el programa debe imprimir en la consola:
+/* Escribe un programa que le pida al usuario su año de nacimiento  e imprima su edad actual en la consola con la frase "Tienes X años". Por ejemplo, asumiendo que el año actual es 2019 y el usuario ingresa 1999, el programa debe imprimir en la consola:
 
 Tienes 29 años */
 
