0b5d13c02d466915f0eacb23ca1bb1f967caa4ea
Sebastian Vega <sebaschanvega@gmail.com>
2023-05-02 18:07:44 -0500

    IMC indice de masa corporal

--- Make It Real 1/ejercicio-05.js
+++ Make It Real 1/ejercicio-05.js
@@ -1,6 +1,6 @@
 /*El índice de masa corporal (IMC), o BMI por sus siglas en inglés, es un valor que determina la cantidad de grasa de una persona.
 
-El BMI se calcula con la siguiente formula:
+El BMI se calcula con la siguiente formula: 
 
 peso / altura^2
 
