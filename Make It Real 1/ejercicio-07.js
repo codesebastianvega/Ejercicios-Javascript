30a30bbd0ef9e1cc631366aef397689d9e6ce5a2
Sebastian Vega <sebaschanvega@gmail.com>
2023-05-06 18:20:12 -0500

    Adivina el numero

--- Make It Real 1/ejercicio-07.js
+++ Make It Real 1/ejercicio-07.js
@@ -1,4 +1,5 @@
-/*Escribe un programa que piense un número de forma aleatoria del 1 al 10 y le pida al usuario que lo trate de adivinar. Si el número es correcto debe imprimir en la consola "Felicitaciones, ese era!", de lo contrario debe imprimir "Lo siento, intenta nuevamente!"*/
+/*Escribe un programa que piense un número de forma aleatoria del 1 al 10 y le pida al usuario que lo trate de adivinar. 
+Si el número es correcto debe imprimir en la consola "Felicitaciones, ese era!", de lo contrario debe imprimir "Lo siento, intenta nuevamente!"*/
 
 let num = parseInt(prompt("Adivina un numero entre 1 y 10"));
 let randomNum = Math.floor(Math.random() * 10) + 1;
