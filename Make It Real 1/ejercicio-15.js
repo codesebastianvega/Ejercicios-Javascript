87ee3851a73ec1cb98eec7f02e4e0e3a800f14f8
Sebastian Vega <sebaschanvega@gmail.com>
2023-05-07 19:31:58 -0500

    Imprimiendo numeros

--- Make It Real 1/ejercicio-15.js
+++ Make It Real 1/ejercicio-15.js
@@ -1,4 +1,4 @@
-/*Escribe un programa para la consola que le pida al usuario un número e imprima los números desde 1 hasta el número.
+/*Escribe un programa para la consola que le pida al usuario un número e imprima los números desde 1 hasta el número.  
 
 Por ejemplo, si el usuario ingresa el número 5, el programa de be imprimir:*/
 
