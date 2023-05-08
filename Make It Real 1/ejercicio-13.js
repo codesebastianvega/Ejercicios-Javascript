682e9e66e2889dd181d8dae36a46ac8875072c8d
Sebastian Vega <sebaschanvega@gmail.com>
2023-05-07 19:30:50 -0500

    Imprimiendo frase 10 veces

--- Make It Real 1/ejercicio-13.js
+++ Make It Real 1/ejercicio-13.js
@@ -1,4 +1,4 @@
-/*Escribe un programa que le pida al usuario ingresar una frase. El programa debe imprimir la frase en la consola 10 veces.
+/*Escribe un programa que le pida al usuario ingresar una frase. El programa debe imprimir la frase en la consola 10 veces.  
 
 Nota: utiliza un ciclo para imprimir la frase las 10 veces.*/
 
