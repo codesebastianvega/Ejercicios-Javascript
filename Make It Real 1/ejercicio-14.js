d7e4378c5c5596b3cb0f39c435c36e7853620490
Sebastian Vega <sebaschanvega@gmail.com>
2023-05-07 19:31:26 -0500

    Imprimiendo frase (n) veces

--- Make It Real 1/ejercicio-14.js
+++ Make It Real 1/ejercicio-14.js
@@ -1,4 +1,4 @@
-/*Escribe un programa que le pida al usuario una frase y un número. El programa debe imprimir la frase en la consola el número de veces que ingresó el usuario.
+/*Escribe un programa que le pida al usuario una frase y un número. El programa debe imprimir la frase en la consola el número de veces que ingresó el usuario.  
 
 Por ejemplo, si el usuario ingresó la frase "Hola Mundo" y el número 5, el programa debe imprimir "Hola Mundo" 5 veces:*/
 
